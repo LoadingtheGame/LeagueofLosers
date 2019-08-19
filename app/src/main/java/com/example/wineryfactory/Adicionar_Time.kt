2929738dbed45505e7b1ff7ec8b53bbd333e674f
LoadingtheGame <loadingthegaming@gmail.com>
2019-08-19 20:42:47 -0300

    commit 2

---
+++ app/src/main/java/com/example/wineryfactory/Adicionar_Time.kt
@@ -0,0 +1,12 @@
+package com.example.wineryfactory
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Adicionar_Time : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicionar__time)
+    }
+}
